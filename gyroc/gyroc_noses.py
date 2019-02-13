a3f7f52bd41674c3de6cc29098e0050353dfd1c8
Haw-minn Lu <hlu@alum.mit.edu>
2019-02-12 16:22:05 -0800

    Add bt60-3in nose designs

--- gyroc/gyroc_noses.py
+++ gyroc/gyroc_noses.py
@@ -1,7 +1,10 @@
 from nosecone_library.specific_noses import BNC20B
-from bodytubes.semroc import bt5, bt2, bt50, bt30, bt55
+from bodytubes.semroc import bt5, bt2, bt50, bt30, bt55, bt60,bt70, bt80, bt40
+from bodytubes.modelrockets_us import _3_00
+
 from misc import utils
 from nosecone.nosecone_bases import HollowBase
+from nosecone.nosecone_threaded_bases import ScrewInBase, ThreadedBaseOutsetScrewInBase
 import os
 
 basedir = os.path.dirname(__file__)
@@ -18,10 +21,30 @@ utils.render_to_file(nc.cone, os.path.join(basedir, "nosecones", "bt20_nose.scad
 nc = BNC20B(scale_bodytube=bt30, thickness = 1/16., shoulder=0.5)
 nc = HollowBase(nc, shoulder=0.5625)
 utils.render_to_file(nc.cone, os.path.join(basedir, "nosecones", "bt30_nose.scad"))
+nc = BNC20B(scale_bodytube=bt40, thickness = 1/16., shoulder=0.5)
+nc = HollowBase(nc, shoulder=0.5625)
+utils.render_to_file(nc.cone, os.path.join(basedir, "nosecones", "bt40_nose.scad"))
 nc = BNC20B(scale_bodytube=bt50, thickness = 1/16., shoulder=0.5)
 nc = HollowBase(nc, shoulder=0.625)
 utils.render_to_file(nc.cone, os.path.join(basedir, "nosecones", "bt50_nose.scad"))
 nc = BNC20B(scale_bodytube=bt55, thickness = 1/16., shoulder=0.5)
 nc = HollowBase(nc, shoulder=0.75)
 utils.render_to_file(nc.cone, os.path.join(basedir, "nosecones", "bt55_nose.scad"))
-
+nc = BNC20B(scale_bodytube=bt60, thickness = 5/64., shoulder=0.5)
+nc = ScrewInBase(nc, shoulder=0.75, thread_height=.375, thread_diameter=1.5, threads_per_inch=10)
+utils.render_to_file(nc.cone,os.path.join(basedir, "nosecones", "bt60_nose.scad"))
+utils.render_to_file(nc.mate, os.path.join(basedir, "nosecones", "bt60_nose_base.scad"))
+nc = BNC20B(scale_bodytube=bt70, thickness = 5/64., shoulder=0.5)
+nc = ScrewInBase(nc, shoulder=0.1, thread_height=.5, thread_diameter=bt70.inner_diameter-.1, threads_per_inch=10)
+utils.render_to_file(nc.cone,os.path.join(basedir, "nosecones", "bt70_nose.scad"))
+utils.render_to_file(nc.mate, os.path.join(basedir, "nosecones", "bt70_nose_base.scad"))
+nc = BNC20B(scale_bodytube=bt80, thickness = 3/32., shoulder=0.5)
+nc = ThreadedBaseOutsetScrewInBase(nc, shoulder=1.0, thread_height=.5, thread_diameter=2.4, lower_tpi=6, upper_tpi=8)
+utils.render_to_file(nc.cone,os.path.join(basedir, "nosecones", "bt80_nose.scad"))
+utils.render_to_file(nc.center_mate, os.path.join(basedir, "nosecones", "bt80_nose_center.scad"))
+utils.render_to_file(nc.mate, os.path.join(basedir, "nosecones", "bt80_nose_base.scad"))
+nc = BNC20B(scale_bodytube=_3_00, thickness = 3/32., shoulder=0.5)
+nc = ThreadedBaseOutsetScrewInBase(nc, shoulder=1.5, thread_height=1., thread_diameter=_3_00.inner_diameter-.2, lower_tpi=6, upper_tpi=8)
+utils.render_to_file(nc.cone,os.path.join(basedir, "nosecones", "3in_nose.scad"))
+utils.render_to_file(nc.center_mate, os.path.join(basedir, "nosecones", "3in_nose_center.scad"))
+utils.render_to_file(nc.mate, os.path.join(basedir, "nosecones", "3in_nose_base.scad"))
