59b73eb7917fe27cc4e711fd18eb75b058f29055
Haw-minn Lu <hlu@westhealth.org>
2020-03-06 13:25:57 -0800

    Include viewscad connector

---
+++ library/viewscad_connector/renderer.py
@@ -0,0 +1,19 @@
+from misc.utils import render as rd
+import viewscad
+import os
+class Renderer():
+    def __init__(self, outdir=None, fn=200):
+        self.fn=fn
+        self.outdir=outdir
+        self.renderer = viewscad.Renderer()
+        
+    def render(self, in_obj, **kw):
+        if hasattr(in_obj, 'cone'):
+            in_obj=in_obj.cone
+        if 'outfile' in kw and self.outdir:
+            kw['outfile']=os.path.join(self.outdir, outfile)
+        if 'dollar_sign_vars' in kw:
+            if not 'fn' in kw.get('dollar_sign_vars',{}):
+                kw['dollar_sign_vars']=dict(**kw.get('dollar_sign_vars',{}), **dict(fn=self.fn))
+        self.renderer.render(in_obj, dollar_sign_vars=dict(fn=self.fn))
+        
